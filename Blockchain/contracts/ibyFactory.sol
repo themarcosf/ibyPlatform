f452ee26d38f87e651cfcb9c71c5a856b5cc0b4a
themarcosf <marcosaurelio.adm@gmail.com>
2022-12-08 11:34:57 -0300

    updated versions of factory and realtyID

--- Blockchain/contracts/ibyFactory.sol
+++ Blockchain/contracts/ibyFactory.sol
@@ -5,19 +5,79 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./helpers.sol";
 
+/**
+ * @title IbyFactory
+ * @author ibyPlatform DevTeam (Bianca Lima, Luiz Kama, Marcos Florencio)
+ *
+ * @notice base contract to manage state owned realty properties, identified by ID
+ * @notice This contract allows creation, update, and read queries.
+ *
+ * @dev this contract is inherited by ibyRealtyID and ibyRealtyNFT
+ */
 contract IbyFactory is Ownable, Helpers {
+    /**
+     * @dev used to avoid counter underflow
+     */
     using SafeMath for uint256;
 
+    /**
+     * @dev event fired whenever a new realty property is created
+     */
     event NewRealty(realtyDescription);
 
-    uint public totalRealtyCounter = 0;
-    realtyDescription[] public totalRealty;
+    /**
+     * @dev event fired whenever a realty property is updated
+     */
+    event UpdatedRealty(realtyDescription);
+
+    /**
+     * @notice realty properties will forever be attached to this contract
+     *         even though future owner may transfer their property directly
+     *         without government actions being required
+     *
+     * @dev visibility public : can be called by ANY contract
+     * @dev TODO : implement function to collect transaction fee
+     */
+    uint public realtyPoolCounter = 0;
+
+    /**
+     * @dev array of all realty properties and their metadata
+     *      see struct realtyDescription
+     * @dev visibility public : can be called by ANY contract
+     */
+    realtyDescription[] public realtyPool;
+
+    /**
+     * @dev maps each realty property's ID to its current owner address
+     * @dev visibility public : can be called by ANY contract
+     */
     mapping(uint => address) public realtyToOwner;
+
+    /**
+     * @dev maps each owner's address to the sum of properties owned
+     * @dev visibility public : can be called by ANY contract
+     */
     mapping(address => uint) public ownerToRealtyCount;
 
+    /**
+     * @dev maps realty property's ID to address approved to handle approval-required functions
+     * @dev visibility private : can be called only from within the contract
+     */
+    mapping(uint => address) private updateApprovals;
+
+    /**
+     * @notice realty property's metadata registered in the blockchain
+     *
+     * @param id : unique ID used throughout the app, ie in smart contracts and nfts
+     * @param lastValuation : last transacted value
+     * @param streetAddress : should include { [street, avenue, ...], number, apartment, etc }
+     * @param legalOwner : current owner address
+     *
+     * @dev TODO : implement valuation tracking
+     */
     struct realtyDescription {
         uint id;
-        uint32 value;
+        uint32 lastValuation;
         string streetAddress;
         string neighborhood;
         string city;
@@ -26,6 +86,18 @@ contract IbyFactory is Ownable, Helpers {
         address legalOwner;
     }
 
+    /**
+     * @notice allow creation of realty properties
+     *         1. generate new id by hashing realty property's core metadata
+     *         2. include a new property in the realty pool
+     *         3. set the gov't address as the initial owner
+     *         4. increase the count of gov't owned properties by 1
+     *         5. increase the realty pool by 1
+     *         6. emit event that new realty property was created
+     *
+     * @dev visibility public : can be called by ANY contract
+     * @dev onlyOwer : only the gov't manager [SPU] is enabled to create a new realty property
+     */
     function createRealty(
         uint32 _value,
         string memory _streetAddress,
@@ -54,32 +126,134 @@ contract IbyFactory is Ownable, Helpers {
             msg.sender
         );
 
-        totalRealty.push(_newRealty);
+        realtyPool.push(_newRealty);
         realtyToOwner[_newRealty.id] = msg.sender;
         ownerToRealtyCount[msg.sender]++;
-        totalRealtyCounter++;
+        realtyPoolCounter++;
         emit NewRealty(_newRealty);
     }
 
-    function updateValue(
-        realtyDescription storage _realty,
-        uint32 _newValue
-    ) internal onlyOwner {
-        _realty.value = _newValue;
+    /**
+     * @notice private API to update owner of a realty property
+     *         1. search all properties until a matching ID is found
+     *         2. store previous owner in scoped variable
+     *         3. update realty property's object
+     *         4. set new owner's address as current owner
+     *         5. increase count of current owner's owned properties by 1
+     *         6. safely decrease count of previous owner's owned properties by 1
+     *         3. emit event that realty was updated
+     * @notice check public API below
+     *
+     * @param _realtyId : object's id stored in the blockchain
+     * @param _newOwner : address of new owner
+     *
+     * @return void : 0 if successful, -1 if matching ID not found
+     *
+     * @dev visibility private : can be called only from within the contract
+     * @dev TODO : implement uint32 overflow handling
+     */
+    function _updateOwner(
+        uint _realtyId,
+        address _newOwner
+    ) private returns (int8 void) {
+        for (uint i = 0; i < realtyPoolCounter; i++) {
+            if (realtyPool[i].id == _realtyId) {
+                address _prevOwner = realtyPool[i].legalOwner;
+                realtyPool[i].legalOwner = _newOwner;
+                realtyToOwner[_realtyId] = _newOwner;
+                ownerToRealtyCount[_newOwner]++;
+                ownerToRealtyCount[_prevOwner] = SafeMath.sub(
+                    ownerToRealtyCount[_prevOwner],
+                    1,
+                    "Error: underflow @IbyFactory._updateOwner"
+                );
+                emit UpdatedRealty(realtyPool[i]);
+                return 0;
+            }
+        }
+        return -1;
     }
 
-    function updateOwner(
-        realtyDescription storage _realty,
-        address _newOwner
-    ) internal onlyOwner {
-        address _prevOwner = _realty.legalOwner;
-        _realty.legalOwner = _newOwner;
-        realtyToOwner[_realty.id] = _newOwner;
-        ownerToRealtyCount[_newOwner]++;
-        ownerToRealtyCount[_prevOwner] = SafeMath.sub(
-            ownerToRealtyCount[_prevOwner],
-            1,
-            "Error: underflow @IbyFactory.updateOwner"
+    /**
+     * @notice private API to update lastValuation parameter of a realty property
+     *         1. search all properties until a matching ID is found
+     *         2. update the realty property's object
+     *         3. emit event that realty was updated
+     * @notice check public API below
+     *
+     * @param _realtyId : object's id stored in the blockchain
+     * @param _newValue : accepts valuations up to 4.0 billion
+     *
+     * @return void : 0 if successful, -1 if matching ID not found
+     *
+     * @dev visibility private : can be called only from within the contract
+     * @dev TODO : implement uint32 overflow handling
+     */
+    function _updateValue(
+        uint _realtyId,
+        uint32 _newValue
+    ) private returns (int8 void) {
+        for (uint i = 0; i < realtyPoolCounter; i++) {
+            if (realtyPool[i].id == _realtyId) {
+                realtyPool[i].lastValuation = _newValue;
+                emit UpdatedRealty(realtyPool[i]);
+                return 0;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * @notice public API to update legalOwner parameter of realty property
+     *         1. require caller to be either owner or to be previouly approved by owner
+     *         2. pass parameters to private API
+     *
+     * @param _realtyId : object's id stored in the blockchain
+     * @param _newOwner : address of new owner
+     *
+     * @dev visibility public : can be called by ANY contract
+     */
+    function updateOwner(uint _realtyId, address _newOwner) public {
+        require(
+            realtyToOwner[_realtyId] == msg.sender ||
+                updateApprovals[_realtyId] == msg.sender
         );
+        _updateOwner(_realtyId, _newOwner);
+    }
+
+    /**
+     * @notice public API to update lastValuation parameter of realty property
+     *         1. require caller to be either owner or to be previouly approved by owner
+     *         2. pass parameters to private API
+     *
+     * @param _realtyId : object's id stored in the blockchain
+     * @param _newValue : accepts valuations up to 4.0 billion
+     *
+     * @dev visibility public : can be called by ANY contract
+     * @dev TODO : implement uint32 overflow handling
+     */
+    function updateValue(uint _realtyId, uint32 _newValue) public {
+        require(
+            realtyToOwner[_realtyId] == msg.sender ||
+                updateApprovals[_realtyId] == msg.sender
+        );
+        _updateValue(_realtyId, _newValue);
+    }
+
+    /**
+     * @notice allow gov't or future owner to delegate option to update realty's object
+     *         1. require only owner can delegate
+     *         2. verify _updater is not the 0 address, ie not a burn
+     *         3. define _updater as approved to update
+     *
+     * @param _updater : receiver of option to update
+     * @param _realtyId : object's id stored in the blockchain
+     *
+     * @dev visibility public : can be called by ANY contract
+     * @dev TODO : implement easy tracking of updater's address
+     */
+    function updateDelegate(address _updater, uint256 _realtyId) public {
+        require(realtyToOwner[_realtyId] == msg.sender);
+        if (_updater != address(0)) updateApprovals[_realtyId] = _updater;
     }
 }
--- Blockchain/contracts/ibyRealtyID.sol
+++ Blockchain/contracts/ibyRealtyID.sol
@@ -5,49 +5,169 @@ import "./ibyFactory.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 /**
- * _tokenId == _realtyId
+ * @title IbyRealtyID
+ * @author ibyPlatform DevTeam (Bianca Lima, Luiz Kama, Marcos Florencio)
+ *
+ * @notice implement ERC721 standard to create unique token for each realty property
  */
-
 contract IbyRealtyID is IbyFactory, ERC721URIStorage {
-    struct nftDescription {
+    /**
+     * @notice counter of tokens created should be same as realtyPoolCounter
+     * @notice tokens will forever be attached to this contract even though future
+     *         owner may transfer directly without government actions being required
+     *
+     * @dev visibility public : can be called by ANY contract
+     * @dev TODO : implement function to collect transaction fee
+     */
+    uint public tokenCounter = 0;
+
+    /**
+     * @dev array of all tokens and their metadata
+     * @dev visibility public : can be called by ANY contract
+     */
+    tokenDescription[] public tokenCollection;
+
+    /**
+     * @dev maps each token ID to its current owner address
+     * @dev visibility public : can be called by ANY contract
+     */
+    mapping(uint => address) public tokenToOwner;
+
+    /**
+     * @dev maps each owner's address to sum of tokens owned
+     * @dev visibility public : can be called by ANY contract
+     */
+    mapping(address => uint) public ownerToTokenCount;
+
+    /**
+     * @dev maps token's ID to address approved to handle approval-required functions
+     * @dev visibility private : can be called only from within contract
+     */
+    mapping(uint => address) private approvals;
+
+    /**
+     * @notice token metadata registered in blockchain
+     *
+     * @param tokenId : same as realtyId deployed using IbyFactory
+     * @param owner : address of nft's owner
+     * @param tokenUri : graphic resource URI on IPFS
+     */
+    struct tokenDescription {
         uint tokenId;
         address owner;
         string tokenUri;
     }
 
-    uint public nftCounter = 0;
-    nftDescription[] public nftCollection;
+    /**
+     * @notice stardard constructor ERC721
+     *         tokenName : Iby Realty ID
+     *         tokenSymbol : IBY
+     */
+    constructor() ERC721("Iby Realty ID", "IBY") {}
 
-    mapping(uint => address) private transferApprovals;
-
-    constructor() ERC721("IbyRealtyID", "IBY") {}
-
+    /**
+     * @notice mints a new token and store metadata
+     *         1. safely mint new token and set gov't address as initial owner
+     *         2. set token URI provided by IPFS [optional]
+     *         3. include new token in token collection
+     *         4. set gov't address as initial owner
+     *         5. increase count of gov't owned tokens by 1
+     *         6. increase token counter by 1
+     *         7. automatically emits a event Transfer(address from, address to, uint256 tokenId)
+     *
+     * @param _tokenId : same as realtyId deployed using IbyFactory
+     * @param _tokenUri : graphic resource URI on IPFS
+     *
+     * @dev visibility public : can be called by ANY contract
+     * @dev onlyOwer : only gov't manager [SPU] is enabled to create a new token
+     */
     function createNft(
-        address _owner,
         uint _tokenId,
         string memory _tokenUri
     ) public onlyOwner {
-        _safeMint(_owner, _tokenId);
+        _safeMint(owner(), _tokenId);
         _setTokenURI(_tokenId, _tokenUri);
-        nftCollection.push(nftDescription(_tokenId, _owner, _tokenUri));
-        nftCounter++;
+        tokenCollection.push(tokenDescription(_tokenId, owner(), _tokenUri));
+        tokenToOwner[_tokenId] = msg.sender;
+        ownerToTokenCount[msg.sender]++;
+        tokenCounter++;
     }
 
+    /**
+     * @notice set tokenUri if none was given at inception
+     *         1. require caller to be either owner or previouly approved by owner
+     *         2. pass parameters to openZeppelin API
+     *
+     * @param _tokenId : same as realtyId deployed using IbyFactory
+     * @param _tokenUri : graphic resource URI on IPFS
+     *
+     * @dev visibility public : can be called by ANY contract
+     */
     function setTokenUri(uint _tokenId, string memory _tokenUri) public {
         require(
-            msg.sender == owner() || msg.sender == transferApprovals[_tokenId],
+            msg.sender == owner() || msg.sender == approvals[_tokenId],
             "Denied @IbyRealtyID.setTokenUri"
         );
         _setTokenURI(_tokenId, _tokenUri);
     }
 
-    function approve(
-        address _spender,
+    /**
+     * @notice private API to transfer token to new owner
+     *         4. set new owner's address as current owner
+     *         5. increase count of current owner's owned tokens by 1
+     *         6. safely decrease count of previous owner's owned properties by 1
+     *         3. emit standard Transfer event
+     * @notice check public API below
+     *
+     * @dev visibility internal : can be called by inherited contracts (openZeppelin declaration)
+     */
+    function _transfer(
+        address _from,
+        address _to,
         uint256 _tokenId
-    ) public virtual override {
+    ) internal override {
+        tokenToOwner[_tokenId] = _to;
+        ownerToTokenCount[_to]++;
+        ownerToTokenCount[_from] = SafeMath.sub(
+            ownerToRealtyCount[_from],
+            1,
+            "Error: underflow @IbyRealtyID._transfer"
+        );
+        emit Transfer(_from, _to, _tokenId);
+    }
+
+    /**
+     * @notice public API to transfer token
+     *         1. require caller to be either owner or to be previouly approved by owner
+     *         2. pass parameters to private API
+     *
+     * @dev visibility public : can be called by ANY contract
+     */
+    function transfer(address _from, address _to, uint256 _tokenId) public {
+        require(
+            tokenToOwner[_tokenId] == msg.sender ||
+                approvals[_tokenId] == msg.sender
+        );
+        if (_to != address(0)) _transfer(_from, _to, _tokenId);
+    }
+
+    /**
+     * @notice allow gov't or future owner to delegate option to transfer token
+     *         1. require only owner can delegate
+     *         2. verify _spender is not 0 address, ie not a burn
+     *         3. define _spender as approved to transfer
+     *         4. emit standard event Approval
+     *
+     * @param _spender : receiver of option to update
+     * @param _tokenId : token id stored in blockchain
+     *
+     * @dev visibility public : can be called by ANY contract
+     * @dev TODO : implement easy tracking of updater's address
+     */
+    function approveTransfer(address _spender, uint256 _tokenId) public {
         require(realtyToOwner[_tokenId] == msg.sender);
         if (_spender != address(0)) {
-            transferApprovals[_tokenId] = _spender;
+            approvals[_tokenId] = _spender;
             emit Approval(msg.sender, _spender, _tokenId);
         }
     }
