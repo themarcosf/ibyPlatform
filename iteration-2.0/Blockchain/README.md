a42682338daadbd0eab12772d25e7e9a2a61d58d
themarcosf <marcosaurelio.adm@gmail.com>
2022-12-16 15:36:23 -0300

    init blockchain structure

---
+++ iteration-2.0/Blockchain/README.md
@@ -0,0 +1,20 @@
+# IBY PLATFORM
+
+This document contains information about tasks required to operate the API.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat help
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+npx hardhat node
+npx hardhat run scripts/deploy.js
+```
+
+Use these commands to compile contracts:
+
+```shell
+npx hardhat compile
+npx hardhat help compile
+```
---
+++ iteration-2.0/Blockchain/contracts/Lock.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+/**
+ * @title Lock
+ * @author hardHat sample contract
+ *
+ * @notice implements a 'vault' for a period of time
+ * @notice only owner can unlock the vault
+ *
+ * @dev payable : TBD
+ */
+contract Lock {
+    uint public unlockTime;
+    address payable public owner;
+
+    event Withdrawal(uint amount, uint when);
+
+    constructor(uint _unlockTime) payable {
+        require(
+            block.timestamp < _unlockTime,
+            "Unlock time should be in the future"
+        );
+
+        unlockTime = _unlockTime;
+        owner = payable(msg.sender);
+    }
+
+    function withdraw() public {
+        require(block.timestamp >= unlockTime, "You can't withdraw yet");
+        require(msg.sender == owner, "You aren't the owner");
+
+        emit Withdrawal(address(this).balance, block.timestamp);
+
+        owner.transfer(address(this).balance);
+    }
+}
