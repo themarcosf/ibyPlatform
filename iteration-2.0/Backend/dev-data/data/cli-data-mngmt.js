ee688eb5450d07d6e888e03addf8f22f908b662b
Marcos Florencio <marcosaurelio.adm@gmail.com>
2022-12-29 04:35:44 -0300

    move dev-data into backend

--- iteration-2.0/Backend/dev-data/data/cli-data-mngmt.js
+++ iteration-2.0/Backend/dev-data/data/cli-data-mngmt.js
@@ -1,15 +1,15 @@
 /**
  * command line script for importing and deleting collections of data
  */
+require("dotenv").config({ path: "./../../../../config.env" });
 
-require("dotenv").config({ path: `${__dirname}/../config.env` });
 const fs = require("fs");
 const mongoose = require("mongoose");
-const Realty = require("../backend/models/realtyModel");
+const Realty = require("./../../models/realtyModel");
 ////////////////////////////////////////////////////////////////////////////////
 
 // remote database
-const DB = process.env.DATABASE.replace(
+const DB = process.env.DATABASE_REMOTE.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD
 );
@@ -18,6 +18,7 @@ const DB = process.env.DATABASE.replace(
 // const DB = process.env.DATABASE_LOCAL;
 
 mongoose
+  .set("strictQuery", false)
   .connect(DB)
   .then((conn) => console.log(`DB Connected: ${conn.connections[0].name}`));
 ////////////////////////////////////////////////////////////////////////////////
@@ -45,10 +46,12 @@ const _delete = async () => {
   process.exit();
 };
 
+// @dev command: node cli-data-mngmt.js --import
 if (process.argv.includes("--import")) {
   _import();
 }
 
+// @dev command: node cli-data-mngmt.js --delete
 if (process.argv.includes("--delete")) {
   _delete();
 }
