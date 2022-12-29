32caf4187fdfe8e03bebdc33e5cef7541e8adf76
Marcos Florencio <marcosaurelio.adm@gmail.com>
2022-12-29 03:23:35 -0300

    overall improvement of auction and realty models

--- iteration-2.0/Backend/models/auctionModel.js
+++ iteration-2.0/Backend/models/auctionModel.js
@@ -1,15 +1,59 @@
+/**
+ * IMPORTANT CAVEAT ABOUT MONGOOSE CUSTOM DATA VALIDATORS:
+ *
+ * inside a validator function (eg images) the THIS keyword
+ * is only gonna point to the current document when a NEW document
+ * is being created ie POST request
+ *
+ * that is not true when UPDATING a document ie PATCH request
+ */
 const mongoose = require("mongoose");
+////////////////////////////////////////////////////////////////////////
 
+/**
+ * @param index {Number}: autoincrement function is in controller
+ * @param leaseBeginDate {Date}: must ref first day of month
+ * @param leaseEndDate {Date} : must ref last day of month
+ */
 const auctionSchema = new mongoose.Schema(
   {
-    realtyId: String,
+    realtyId: {
+      type: mongoose.Schema.ObjectId,
+      required: [true, "realtyId is required"],
+      ref: "Realty",
+    },
     index: Number,
-    minValue: Number,
-    currentValue: Number,
-    leaseBeginDate: Date,
-    leaseEndDate: Date,
+    minAskValue: Number,
     auctionEndDate: Date,
-    auctionLog: [Object],
+    leaseBeginDate: {
+      type: Date,
+      validate: {
+        validator: function (val) {
+          let _isValid;
+          if (val <= this.auctionEndDate) _isValid = false;
+          return _isValid;
+        },
+        message: "lease must begin after auction end date",
+      },
+    },
+    leaseEndDate: {
+      type: Date,
+      validate: {
+        validator: function (val) {
+          let _isValid;
+          if (val <= this.leaseBeginDate) _isValid = false;
+          return _isValid;
+        },
+        message: "lease must end after lease begin date",
+      },
+    },
+    auctionLog: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+        bidValue: Number,
+      },
+    ],
     active: {
       type: Boolean,
       default: true,
@@ -25,6 +69,22 @@ const auctionSchema = new mongoose.Schema(
     toObject: { virtuals: true },
   }
 );
+////////////////////////////////////////////////////////////////////////
+
+/**
+ * Virtual Properties:
+ * enables separation between business logic and application logic
+ * derivative fields not to be persisted in the database
+ * cannot be manipulated in queries eg Auction.find( $where: { LeaseDurationMonths: 1 })
+ */
+auctionSchema.virtual("LeaseDurationMonths").get(function () {
+  let months;
+  months =
+    (this.leaseEndDate.getFullYear() - this.leaseBeginDate.getFullYear()) * 12;
+  months -= this.leaseBeginDate.getMonth();
+  months += this.leaseEndDate.getMonth();
+  return ++months;
+});
 ////////////////////////////////////////////////////////////////////////
 
 const Auction = new mongoose.model("Auction", auctionSchema);
--- iteration-2.0/Backend/models/realtyModel.js
+++ iteration-2.0/Backend/models/realtyModel.js
@@ -1,13 +1,85 @@
+/**
+ * IMPORTANT CAVEAT ABOUT MONGOOSE CUSTOM DATA VALIDATORS:
+ *
+ * inside a validator function (eg images) the THIS keyword
+ * is only gonna point to the current document when a NEW document
+ * is being created ie POST request
+ *
+ * that is not true when UPDATING a document ie PATCH request
+ */
 const mongoose = require("mongoose");
+////////////////////////////////////////////////////////////////////////
 
 const realtySchema = new mongoose.Schema(
   {
-    address: String,
-    district: String,
-    city: String,
-    state: String,
-    description: String,
-    images: [String],
+    address: {
+      type: String,
+      unique: [true, "address already exists"],
+      required: [true, "address is required"],
+      minLength: [5, "invalid length"],
+      maxLengh: [40, "invalid lengh"],
+      trim: true,
+    },
+    district: {
+      type: String,
+      required: [true, "district is required"],
+      minLength: [2, "invalid length"],
+      maxLengh: [40, "invalid lengh"],
+      trim: true,
+    },
+    city: {
+      type: String,
+      required: [true, "city is required"],
+      minLength: [2, "invalid length"],
+      maxLengh: [40, "invalid lengh"],
+      trim: true,
+    },
+    state: {
+      type: String,
+      enum: ["ES", "MG", "RJ", "SP"],
+      required: [true, "state is required"],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "description is required"],
+      minLength: [50, "invalid length"],
+      maxLengh: [500, "invalid lengh"],
+      trim: true,
+    },
+    /**
+     * mongoDB supports geospatial data coords
+     * standard: GeoJSON [long, lat]
+     */
+    geoLocation: {
+      type: {
+        type: String,
+        default: "Point",
+        enum: ["Point"],
+      },
+      coordinates: [Number],
+    },
+    imageCover: {
+      type: String,
+      required: [true, "imageCover is required"],
+      minlength: [4, "lengthmin is 4 digits"],
+      maxlength: [20, "lengthmax is 20 digits"],
+      trim: true,
+    },
+    images: {
+      type: [String],
+      validate: {
+        validator: function (val) {
+          let _isValid;
+          val.forEach((el) => {
+            if (el !== el.trim() || el.length < 4 || el.length > 20)
+              _isValid = false;
+          });
+          return _isValid;
+        },
+        message: "invalid path: {VALUE}",
+      },
+    },
     sqMeters: Number,
     inConstruction: Boolean,
     toRetrofit: Boolean,
@@ -15,6 +87,10 @@ const realtySchema = new mongoose.Schema(
       type: Boolean,
       default: true,
       select: false,
+    },
+    vip: {
+      type: Boolean,
+      default: false,
     },
     createdAt: {
       type: Date,
