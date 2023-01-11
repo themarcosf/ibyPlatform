8ec0099e616b402f50de989834419b240f680c4a
Marcos Florencio <marcosaurelio.adm@gmail.com>
2023-01-11 11:50:58 -0300

    add currentUser routes

--- iteration-2.0/Backend/controllers/authController.js
+++ iteration-2.0/Backend/controllers/authController.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const { promisify } = require("util");
 const User = require("./../models/userModel");
 const { CustomError } = require("./../utils/errors");
@@ -52,17 +53,40 @@ exports.login = asyncHandler(async function (req, res, next) {
 
 /**
  * authentication middleware : handles authentication before routes access
- * authorization middleware : handles authorization to specific routes by certain users eg admin
+ *
+ * TODO : validate Google OAuth token
  *
  * PROMISIFY (fn) : node built-in method to avoid callback pattern in async/await functions
  */
 exports.authentication = asyncHandler(async function (req, res, next) {
+  const _error = new CustomError("Authentication failed", 401);
+  // check headers for authorization token
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer")) return next(_error);
+
+  const token = authHeader.split(" ")[1];
+
+  // validate token integrity
+  const _payload = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  // verify user status
+  const _user = await User.findById(_payload.id)
+    .select("+nationalId")
+    .select("+mobile")
+    .select("+status")
+    .select("+role");
+
+  if (!_user || _user.status === "inactive") return next(_error);
+
+  // attach user data to request
+  req.user = _user;
   next();
 });
 
 /**
- * PASSING ARGUMENTS INTO MIDDLEWARE FUNCTIONS
+ * authorization middleware : handles authorization to specific routes by certain users eg admin
  *
+ * PASSING ARGUMENTS INTO MIDDLEWARE FUNCTIONS
  * 1. create wrapper function that returns middleware function
  * 2. middleware gets access to wrapper function parameters due to closure
  */
