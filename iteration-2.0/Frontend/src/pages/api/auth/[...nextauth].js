92a99a0d69660c61f2e27108af3313589d294d45
Marcos Florencio <marcosaurelio.adm@gmail.com>
2023-01-19 13:25:19 -0300

    fix merge conflicts

--- iteration-2.0/Frontend/src/pages/api/auth/[...nextauth].js
+++ iteration-2.0/Frontend/src/pages/api/auth/[...nextauth].js
@@ -1,18 +1,46 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export default NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
+const nextAuthOptions = (req, res) => {
+  return {
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        authorization: {
+          params: {
+            prompt: "consent",
+            access_type: "offline",
+            response_type: "code",
+          },
         },
+      }),
+    ],
+    callbacks: {
+      async jwt({ token, user, account }) {
+        console.log(account);
+        if (account) {
+          const response = await fetch(
+            "http://127.0.0.1:8000/api/v1/user/login",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${account.access_token}`,
+              },
+            }
+          );
+          const cookies = response.headers.get("set-cookie");
+
+          res.setHeader("Set-Cookie", cookies, {
+            httpOnly: false,
+          });
+        }
       },
-    }),
-  ],
-});
+    },
+  };
+};
+
+export default (req, res) => {
+  return NextAuth(req, res, nextAuthOptions(req, res));
+};
