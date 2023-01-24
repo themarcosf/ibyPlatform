3b5c2a8396024ca6487956dba49a7d9cbbdf5bdc
Marcos F <84358344+themarcosf@users.noreply.github.com>
2023-01-24 08:04:01 -0300

    init commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["lh3.googleusercontent.com", "classic.exame.com"],
+  },
+}
+
+module.exports = nextConfig
