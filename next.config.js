e0a645360f6ee76fa083cae8b145b7b359f67620
Marcos Florencio <marcosaurelio.adm@gmail.com>
2023-01-25 18:58:41 -0300

    change devDependency react-intl-currency-input and comment out next.config process.env.BASEURL

--- next.config.js
+++ next.config.js
@@ -4,9 +4,9 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
-  env: {
-    BASEURL: "https://enigmatic-cove-57254.herokuapp.com/api/v1",
-  },
+  // env: {
+  //   BASEURL: "https://enigmatic-cove-57254.herokuapp.com/api/v1",
+  // },
 };
 
 module.exports = nextConfig;
