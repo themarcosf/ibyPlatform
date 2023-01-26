044e7c7960c0767d988e3e9bd94f761f0d2f7d32
Luiz Felipe <68920578+luiz-k-alencar@users.noreply.github.com>
2023-01-26 09:02:04 -0300

    Update getUserData.js

--- src/functions/getUserData.js
+++ src/functions/getUserData.js
@@ -3,6 +3,8 @@ import cookieCutter from "cookie-cutter";
 export async function getUserData() {
   const jwtCookie = cookieCutter.get("jwt");
 
+  console.log(process.env)
+
   const response = await fetch(`https://enigmatic-cove-57254.herokuapp.com/api/v1/user/me`, {
     method: "GET",
     headers: {
