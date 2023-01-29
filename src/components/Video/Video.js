ae4e672b47972e655da4d11be465cdf551bfcc0e
Luiz Felipe <68920578+luiz-k-alencar@users.noreply.github.com>
2023-01-29 17:10:58 -0300

    Update Video.js

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -7,7 +7,7 @@ const Video = () => {
       height="450px"
       className={styles.video}
       style={{ justifySelf: "center" }}
-      src="https://www.youtube.com/embed/nm-eVnHVMFk"
+      src="https://www.youtube.com/embed/ffRHwcuqcRA"
       title="YouTube video player"
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
